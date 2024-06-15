55c0ab5fd4473461b843af4fe993effa888aeb2d
Guilhermesantol <162186707+Guilhermesantol@users.noreply.github.com>
2024-06-15 15:23:26 -0300

    Update excluir.php

--- excluir.php
+++ excluir.php
@@ -9,28 +9,3 @@ if (isset($_GET["id"])){
     require 'pagina.php';
 
 }
-
-?>
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EXERCICIO</title>
-    <link rel="stylesheet" href="styles2.css">
-</head>
-<body>
-<h1>Deletar</h1>
-<main>
-<form action="excluir.php" method="post">
-    <label for="id">Digite o ID:</label>
-    <input type="int" id="id" name="id-input"> <br>
-    <a href="pagina.php">voltar</a>
-
-
-                    <input type="submit" value="Deletar" id="enviar"> <br>
-</form>
-    </main>
-
-
-
